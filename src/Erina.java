ebba198217d01ba95edab4d59abc4834203c5c08
liueri19 <liueri19@7hills.org>
2018-05-10 15:11:52 -0400

    reformatted code

--- src/Erina.java
+++ src/Erina.java
@@ -1,13 +1,16 @@
-import greenfoot.*;
-import java.util.*;
+import greenfoot.GreenfootSound;
+import greenfoot.World;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
  * The Erina is the perfected version of the original beloved and wildly popular Arena.
  * This is the World where Competitors compete in.
  * The Erina aims to:
- *		Force the Competitors to comply with requirements on method usage,
- *		Keep it easy to implement Competitors,
- *		Resolve the hitter hittee issue that plagued the original Arena.
+ * Force the Competitors to comply with requirements on method usage,
+ * Keep it easy to implement Competitors,
+ * Resolve the hitter hittee issue that plagued the original Arena.
  */
 public final class Erina extends World {
 	
@@ -19,29 +22,33 @@ public final class Erina extends World {
 	 * methods. It is also important to override (or in some other way limit) any additional 
 	 * methods that may be added to Greenfoot in a future Greenfoot release.
 	 */
-	
+
 	private static final int WORLD_WIDTH = 1024;
-    private static final int WORLD_HEIGHT = 720;
-    
-    private static final String BGM_FILENAME = "resources/17 Disc Wars 1.wav";
-    private static final GreenfootSound BGM = new GreenfootSound(BGM_FILENAME);
-    
-    public Erina() {
-    	super(WORLD_WIDTH, WORLD_HEIGHT, 1);
-		
-    	System.out.println("Welcome to The Erina!");
-    }
-    
-    
-    @Override
-    public void act() {
-		
-    }
-	
-	
+	private static final int WORLD_HEIGHT = 720;
+
+	private static final String BGM_FILENAME = "resources/17 Disc Wars 1.wav";
+	private static final GreenfootSound BGM = new GreenfootSound(BGM_FILENAME);
+
+	public Erina() {
+		super(WORLD_WIDTH, WORLD_HEIGHT, 1);
+
+		System.out.println("Welcome to The Erina!");
+	}
+
+
 	@Override
-	public <T> List<T> getObjects(Class<T> cls) { return Collections.emptyList(); }
-	
+	public void act() {
+
+	}
+
+
 	@Override
-	public <T> List<T> getObjectsAt(int x, int y, Class<T> cls) { return Collections.emptyList(); }
+	public <T> List<T> getObjects(Class<T> cls) {
+		return Collections.emptyList();
+	}
+
+	@Override
+	public <T> List<T> getObjectsAt(int x, int y, Class<T> cls) {
+		return Collections.emptyList();
+	}
 }

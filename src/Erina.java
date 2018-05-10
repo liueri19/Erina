77c50fe932660a290398f6b057b8782958e65a9c
liueri19 <liueri19@7hills.org>
2018-05-10 14:44:36 -0400

    Added comments and overrode some methods

--- src/Erina.java
+++ src/Erina.java
@@ -1,6 +1,25 @@
 import greenfoot.*;
+import java.util.*;
 
+/**
+ * The Erina is the perfected version of the original beloved and wildly popular Arena.
+ * This is the World where Competitors compete in.
+ * The Erina aims to:
+ *		Force the Competitors to comply with requirements on method usage,
+ *		Keep it easy to implement Competitors,
+ *		Resolve the hitter hittee issue that plagued the original Arena.
+ */
 public final class Erina extends World {
+	
+	/*
+	 * Note to future maintainers:
+	 * In order to limit the Competitors from accessing certain methods, those methods have been
+	 * overriden. It is important to pervent Competitors from gaining access to instances of super
+	 * types (such as Actor, World), as such instances would enable the Competitors to call illegal 
+	 * methods. It is also important to override (or in some other way limit) any additional 
+	 * methods that may be added to Greenfoot in a future Greenfoot release.
+	 */
+	
 	private static final int WORLD_WIDTH = 1024;
     private static final int WORLD_HEIGHT = 720;
     
@@ -9,13 +28,20 @@ public final class Erina extends World {
     
     public Erina() {
     	super(WORLD_WIDTH, WORLD_HEIGHT, 1);
-    	
+		
     	System.out.println("Welcome to The Erina!");
     }
     
     
     @Override
     public void act() {
-    	
+		
     }
+	
+	
+	@Override
+	public <T> List<T> getObjects(Class<T> cls) { return Collections.emptyList(); }
+	
+	@Override
+	public <T> List<T> getObjectsAt(int x, int y, Class<T> cls) { return Collections.emptyList(); }
 }

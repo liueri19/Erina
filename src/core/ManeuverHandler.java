acfef45a64eca8fcc1abb1ef437bc0d6a031683d
liueri19 <liueri19@7hills.org>
2018-05-21 11:26:42 -0400

    added Referee, ManeuverHandler

---
+++ src/core/ManeuverHandler.java
@@ -0,0 +1,18 @@
+package core;
+
+/**
+ * This class handles Maneuvers supplied by Competitors.
+ *
+ * @version alpha
+ * @author Eric
+ */
+final class ManeuverHandler {
+	private ManeuverHandler() {}
+
+	/**
+	 * Handles the specified Maneuver and apply it on the specified Competitor.
+	 */
+	static void handle(Competitor competitor, Maneuver maneuver) {
+		// TODO implement this
+	}
+}
